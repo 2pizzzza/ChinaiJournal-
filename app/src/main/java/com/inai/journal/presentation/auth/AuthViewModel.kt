02f7426905a7e05cd0e41d6670b264459ed0e61c
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-05-13 08:54:26 +0600

    fixed bug with hilt and grpc

--- app/src/main/java/com/inai/journal/presentation/auth/AuthViewModel.kt
+++ app/src/main/java/com/inai/journal/presentation/auth/AuthViewModel.kt
@@ -3,6 +3,5 @@ package com.inai.journal.presentation.auth
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class AuthViewModel : ViewModel() {
 }
