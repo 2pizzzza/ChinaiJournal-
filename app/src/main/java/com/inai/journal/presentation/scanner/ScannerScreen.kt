8967c848950e3f6e48f0d0fc6c8abad37d828094
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-05-31 04:43:46 +0600

    scanner qr code

--- app/src/main/java/com/inai/journal/presentation/scanner/ScannerScreen.kt
+++ app/src/main/java/com/inai/journal/presentation/scanner/ScannerScreen.kt
@@ -2,9 +2,11 @@ package com.inai.journal.presentation.scanner
 
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +15,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.inai.journal.R
 import com.journeyapps.barcodescanner.ScanContract
 import com.journeyapps.barcodescanner.ScanOptions
 
@@ -37,7 +42,12 @@ fun ScannerScreen(){
     )
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = qrcode.value)
+            Image(
+                painter = painterResource(id = R.drawable.qr_code_scanner),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(300.dp)
+                )
             Button(onClick = {
                 val options = ScanOptions()
                 options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
